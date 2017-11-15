2c5a7689e828acadbcec49fc6257ec31f1e2da1a
yjosh03 <33265167+yjosh03@users.noreply.github.com>
2017-11-15 17:32:39 -0500

    Add files via upload
    start map with changed path way and organized folders

---
+++ src/overlays/research_institutions.js
@@ -0,0 +1,10 @@
+var institutions ={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Institutio": "National Fisheries Resources Research Institute, Jijna, Uganda (NaFIRRI)" }, "geometry": { "type": "Point", "coordinates": [ 33.207678, 0.416974 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Institutio": "Tanzania Fisheries Research Institute, Ilemela, Tanzania (TAFIRI)" }, "geometry": { "type": "Point", "coordinates": [ 32.8882537,-2.5607068 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Institutio": "Kenya Marine and Fisheries Reserach Institute, Kisumu, Kenya (KMFRI)" }, "geometry": { "type": "Point", "coordinates": [ 34.7370179, -0.0883357 ] } }
+]
+};
